688cb96ca42c69e4660454d1a234144f65b9a6d7
Your Name <you@example.com>
2016-10-04 16:23:40 +0800

    aaa

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
-render(<App />, document.getElementById('root'));
+import { renderRoutes } from './routes';
+render(renderRoutes(), document.getElementById('root'));
