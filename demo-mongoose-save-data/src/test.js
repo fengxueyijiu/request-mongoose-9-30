d94fbc4c8af7799b83e85275d3171f4574447c7b
Your Name <you@example.com>
2016-10-07 14:50:52 +0800

    aa

--- demo-mongoose-save-data/src/test.js
+++ demo-mongoose-save-data/src/test.js
@@ -1,40 +1,30 @@
 var express = require('express');
-var app=express();
-var bodyParser=require('body-parser');    //3-4行首先装包bodyParser.然后用于命令行模拟curl -H详情笔记本  (req.body也需要)
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))    //yongyu浏览器地址栏：localhost:3000/write
-
+var app = express();
 var mongoose=require('mongoose');
-mongoose.Promise=global.Promise;   //清楚过期警告
-mongoose.connect('mongodb://localhost:27017/request-mongoose-9-30');
-var db=mongoose.connection;
-var Schema=mongoose.Schema;
+mongoose.connect('mongodb://localhost:27017/digicity-express-api')
+// var db=mongoose.connection;
+var Schema = mongoose.Schema;
+var PostSchema = new Schema(
+  {
+    name:String,
+    category:String,
+    content:String
 
+  },
+  {
+    timestamps:true
+  }
+)
+var catty= mongoose.model('Cat',PostSchema);
+var cat=new catty({name:'mony'});   //实例化
+console.log(cat.name);
+cat.save();
 
-var cors=require('cors');  //关闭同源策略，开放cors   装cors包
-app.use(cors());
+app.post('/posts', function(req, res){
+  console.log('hello');
 
-db.on('error',console.log);
-db.once('open',function(){
-  console.log('success!')
-  var PostSchema=new Schema(
-    {
-    name:String
-  });
-  var cattys=mongoose.model('cat',PostSchema);
-  var catty = new cattys({name:'lucky'}); //实例化
-  console.log(catty.name);
-  catty.save()
+});
 
-
-})
-
-
-
-
-
-
-
-app.listen(3000,function(){
-  console.log('running on port 3000...')
-})
+app.listen(3000, function(){
+  console.log('running on port 3000.....');
+});
